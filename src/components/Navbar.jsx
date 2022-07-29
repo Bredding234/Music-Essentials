8215ab860a4e1e950c8df0bcd0cc01cb0a78e570
Amka <Aminhassen1470@gmail.com>
2022-07-29 10:21:08 -0400

    create navbar, install react-icons, tailwindcss postcss autoprefixer and add bootsrtap

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+
+const Navbar = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+	let navbar;
+	if (isLoggedIn) {
+		navbar = (
+			<nav className='navbar navbar-dark bg-dark'>
+				<div className='container flex justify-between items-center '>
+					<h1 className='fs-3 text-primary'>
+						<a href='#'>
+							Music <span className='text-white'>Essentials</span>
+						</a>
+					</h1>
+					<div className='flex items-center gap-4'>
+						<ul className='flex items-center gap-5 text-white'>
+							<li>
+								<a href='#'>Home</a>
+							</li>
+							<li>
+								<a href='#'>Music</a>
+							</li>
+							<li>
+								<a href='#'>Store</a>
+							</li>
+							<li>
+								<a href='#'>News</a>
+							</li>
+						</ul>
+						<div>
+							<FaUserCircle color='white' size={30} />
+						</div>
+					</div>
+				</div>
+			</nav>
+		);
+	} else {
+		navbar = (
+			<nav className='navbar navbar-dark bg-dark'>
+				<div className='container text-center justify-content-center'>
+					<h1 className='fs-3'>
+						<a href='#'>
+							Music <span className='text-white'>Essentials</span>
+						</a>
+					</h1>
+				</div>
+			</nav>
+		);
+	}
+
+	return <>{navbar}</>;
+};
+
+export default Navbar;
